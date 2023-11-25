52880b6e5553ad064701687f09e185fcddb80bd6
Alan Bogov (@avavion) <avavionmvm@gmail.com>
2023-11-25 17:48:44 +0300

    fix: features

--- src/app/page.js
+++ src/app/page.js
@@ -3,22 +3,21 @@ import ServicesSlider from "@/components/ServicesSlider/ServicesSlider";
 import Projects from "@/components/Projects/Projects";
 import Detailing from "@/components/Detailing/Detailing";
 import Popular from "@/components/Populars/Popular";
-import Features from "@/components/Features/features";
+import Features from "@/components/Features/Features";
 import Reviews from "@/components/Reviews/Reviews";
 import Contatcs from "@/components/Contacts/Contatcs";
-
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <ServicesSlider />
-      <Projects/>
-      <Detailing/>
-      <Popular/>
-      <Features/>
-      <Reviews/>
-      <Contatcs/>
+      <Projects />
+      <Detailing />
+      <Popular />
+      <Features />
+      <Reviews />
+      <Contatcs />
     </main>
   );
 }
